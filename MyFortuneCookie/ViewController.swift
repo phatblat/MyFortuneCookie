fc54282360d1e68f5493d8fdd9cf470f4617f70e
Ben Chatelain <ben@octop.ad>
2017-07-27 14:22:16 -0600

    Wire up button and label

--- MyFortuneCookie/ViewController.swift
+++ MyFortuneCookie/ViewController.swift
@@ -9,17 +9,24 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    @IBOutlet var fortuneLabel: UILabel!
 }
 
+// MARK: - UIViewController
+extension ViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        fortuneLabel.text = ""
+    }
+}
+
+// MARK: - IBActions
+extension ViewController {
+    @IBAction func fortuneButtonWasTapped(_ sender: Any) {
+        print("button tapped")
+        fortuneLabel.text = "button tapped"
+    }
+}
